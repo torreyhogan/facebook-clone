dc63aa326fc7328c8579e6855070398346a9bb30
Torrey Hogan <hogantorrey@gmail.com>
2019-01-26 14:17:49 -0800

    more sendgrid changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,16 +95,8 @@ Rails.application.configure do
   # SENDGRID SETTINGS
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'powerful-cliffs-33601.herokuapp.com.herokuapp.com'
+  host = 'powerful-cliffs-33601.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+ 
 
 end
